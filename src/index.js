a61ef0b1d317c61f052b88e3967d81052e43ac96
ShaunBinoy <shaunpbinoyoff@gmail.com>
2023-12-19 10:52:50 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// const { log } = require("console")
+const express = require("express")
+const app =express()
+const path = require("path")
+const hbs = require("hbs")
+const LogInModel = require("./mongodb")
+
+const tempelatePath = path.join(__dirname,'../tempelates')
+
+app.use(express.json())
+app.set("view engine","hbs")
+app.set("views",tempelatePath)
+app.use(express.urlencoded({extended:false}))
+
+
+app.get("/",(req,res)=>{
+    res.render("login")
+})
+
+app.get("/signup",(req,res)=>{
+    res.render("signup")
+})
+
+app.post("/signup", async (req,res)=>{
+
+    const data={
+        name:req.body.name,
+        password:req.body.password
+    }
+
+    // try {
+    //     await LogInModel.insertMany([data], { wtimeout: 1000 });
+    //     res.render("home");
+    // } catch (error) {
+    //     console.error("Error inserting data:", error);
+    //     res.status(500).send("Internal Server Error");
+    // }
+    
+
+    await LogInModel.insertMany([data])
+
+    res.render("home")
+    
+})
+
+app.listen(3000,()=>{
+    console.log("Port Connected");
+})
---
+++ src/mongodb.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/LoginSignUp")
+.then(()=>{
+    console.log("Mongo Connected");
+})
+
+.catch((err)=>{
+    console.log("Failed to Connect",err);
+})
+
+const LogInSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+
+const LogInModel = new mongoose.model("LoginCollection", LogInSchema);
+
+module.exports=LogInModel;
